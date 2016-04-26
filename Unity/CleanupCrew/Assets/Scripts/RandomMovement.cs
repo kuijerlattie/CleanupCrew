0a8e12e6ed0e341a2adfb68c17373f5c2d2a44c4
jossepr8 <jossepr8@hotmail.com>
2016-04-26 13:48:33 +0200

    sync

---
+++ Unity/CleanupCrew/Assets/Scripts/RandomMovement.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomMovement : MonoBehaviour {
+
+    float force = 100f;
+    Vector3 direction;
+	// Use this for initialization
+	void Start () {
+        direction = new Vector3(Random.Range(-1f, 1f), 0, Random.Range(-1f, 1f)).normalized;
+        GetComponent<Rigidbody>().AddForce(direction * force);
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+}
