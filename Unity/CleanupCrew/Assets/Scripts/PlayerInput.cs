f652f27ee7b922f4714f7051eb8560352e6b4dd2
jossepr8 <jossepr8@hotmail.com>
2016-04-28 13:53:01 +0200

    spawning and clicking

---
+++ Unity/CleanupCrew/Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInput : MonoBehaviour {
+
+    bool pressedOnce = false;
+    public Rect spawnLocation;
+    public int AmountOfSpheres = 100;   //set in inspector probably
+	// Use this for initialization
+	void Start () {
+        SpawnSpheres.SpawnMultipleSpheres(spawnLocation, AmountOfSpheres);
+	}
+
+    void Reset()
+    {
+        pressedOnce = false;
+    }
+
+    static public bool GetInput()
+    {
+        return Input.GetMouseButtonDown(0);
+    }
+    
+	
+	// Update is called once per frame
+	void Update () {
+	    if(GetInput() && !pressedOnce)
+        {
+            pressedOnce = true;
+            Vector3 mousepos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Debug.Log(mousepos);
+            SpawnSpheres.SpawnSphere(mousepos, true);
+        }
+	}
+}
---
+++ Unity/CleanupCrew/Assets/SpawnSpheres.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnSpheres
+{ 
+    static public void SpawnSphere(Vector3 position, bool ImmidiateExplode = false)
+    {
+        position.y = 0;
+        GameObject spawnedSphere = GameObject.Instantiate(Resources.Load("Prefabs/Ball") as GameObject);
+        spawnedSphere.transform.position = position;
+        spawnedSphere.GetComponent<Explosion>().enabled = ImmidiateExplode;
+    }
+
+    static public void SpawnMultipleSpheres(Rect spawnArea, int amount)
+    {
+        float Root = Mathf.Sqrt(amount);
+        int roundedRoot = (int)Root;
+        for (int ix = 0; ix < roundedRoot; ix++)
+        {
+            for (int iy = 0; iy < roundedRoot; iy++)
+            {
+                Vector3 spawnloc = new Vector3(spawnArea.width / roundedRoot * ix + spawnArea.x, 0, spawnArea.height / roundedRoot * iy + spawnArea.y);
+                SpawnSphere(spawnloc, false);
+            }
+        }
+    }
+
+}
