d85e70f2f3cb70e93137d3dca37c7cc6c14f75b0
Bart <kuijerlattie@gmail.com>
2016-04-28 14:05:51 +0200

    paddle script

---
+++ Unity/CleanupCrew/Assets/PaddleScript.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PaddleScript : MonoBehaviour {
+
+    Transform trans;
+    Vector3 mousepos;
+	// Use this for initialization
+	void Start () {
+        trans = GetComponent<Transform>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    //x
+        mousepos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        trans.position = new Vector3(mousepos.x, trans.position.y, trans.position.z);
+	}
+}
