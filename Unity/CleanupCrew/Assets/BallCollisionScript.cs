60f11aa9d8107ffc6c28eb0234afb8b5199cc328
Ari-Pekka Åttman <apattman@gmail.com>
2016-05-09 14:27:37 +0200

    Round wall + collision detection

---
+++ Unity/CleanupCrew/Assets/BallCollisionScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BallCollisionScript : MonoBehaviour {
+    
+    public bool touched;
+    
+    GameObject ball;
+    
+	// Use this for initialization
+	void Start () {
+        touched = false;
+        ball = GameObject.FindGameObjectWithTag("Ball");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	    if(touched == true)
+        {
+            Physics.IgnoreLayerCollision(10, 9, true);
+        }
+	}
+
+    void OnCollisionEnter(Collision other)
+    {
+        Debug.Log(other.gameObject.tag);
+        if(other.gameObject.tag == "paddle")
+        {
+            touched = true;
+            gameObject.layer = LayerMask.NameToLayer("WallIgnoring");
+        }
+    }
+}
