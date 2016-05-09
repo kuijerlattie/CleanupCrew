bff1813f48811e53dfa10447987332cee712650e
jossepr8 <jossepr8@hotmail.com>
2016-05-09 12:29:04 +0200

    rotating paddle

---
+++ Unity/CleanupCrew/Assets/rotationtest.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotationtest : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	   if(Input.GetKey(KeyCode.A))
+        {
+            gameObject.transform.RotateAround(Vector3.up, 0.1f);
+        }
+	}
+}
