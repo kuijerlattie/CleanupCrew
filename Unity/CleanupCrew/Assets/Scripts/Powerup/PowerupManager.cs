20bf97b86972b8bff9e9d9bdc27f2114e33dcba0
Bart <kuijerlattie@gmail.com>
2016-05-11 23:29:40 +0200

    powerup activator
    powerups are managed trough the powerup manager. the enum in the top
    will contain names for all the available powerups.
    the Public void ActivatePowerup(PowerupType type) will do a switch on
    the powerup that should be activated. if a powerup is not in there, it
    will not work.
    powerups will automaticly do a callback to DeactivatePowerup that works
    the same but removes the powerup you currently have active.

---
+++ Unity/CleanupCrew/Assets/Scripts/Powerup/PowerupManager.cs
@@ -0,0 +1,82 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerupManager : MonoBehaviour {
+
+    public GameObject[] PowerupList;
+    float timer = 0;
+    public float spawntimerInSeconds = 25;
+    public float spawntimerRandomDifference = 10;
+    public bool spawnFromCenter = true;
+
+    public enum PowerupType
+    {
+        MultiPaddle
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        ResetSpawnTimer();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        if (timer <= 0)
+        {
+            SpawnRandomPowerup();
+            ResetSpawnTimer();
+        }
+
+        timer -= Time.deltaTime;
+    }
+
+    void ResetSpawnTimer()
+    {
+        timer = spawntimerInSeconds + (Random.Range(0, spawntimerRandomDifference * 2) - spawntimerRandomDifference);
+    }
+
+    void SpawnRandomPowerup()
+    {
+        Vector3 spawnlocation = new Vector3(0, 0, 0);
+        if (!spawnFromCenter)
+        {
+            //do other location logic here
+        }
+        GameObject powerup = (GameObject)Instantiate(PowerupList[Random.Range(0, PowerupList.Length)], spawnlocation, Quaternion.identity);
+        //give powerup a outward force;
+    }
+
+    public void ActivatePowerup(PowerupType type)
+    {
+        switch (type)
+        {
+            case PowerupType.MultiPaddle:
+                //activate powerup here.
+                
+                break;
+            default:
+                break;
+        }
+    }
+
+    public void DeactivatePowerup(PowerupType type)
+    {
+        switch (type)
+        {
+            case PowerupType.MultiPaddle:
+                //deactivate powerup here.
+                break;
+            default:
+                break;
+        }
+    }
+
+    IEnumerator PowerupTimer(PowerupType type, float durationInSeconds)
+    {
+        yield return new WaitForSeconds(durationInSeconds);
+        DeactivatePowerup(type);
+    }
+}
---
+++ Unity/CleanupCrew/Assets/Scripts/Powerup/PowerupStartScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PowerupStartScript : MonoBehaviour {
+
+    PowerupManager pManager;
+    public PowerupManager.PowerupType powerupType = PowerupManager.PowerupType.MultiPaddle;
+	// Use this for initialization
+	void Start () {
+        pManager = FindObjectOfType<PowerupManager>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.tag == "paddle")
+        {
+            pManager.ActivatePowerup(powerupType);
+        }
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Wall"))
+        {
+            GameObject.Destroy(gameObject);
+        }
+    }
+}
