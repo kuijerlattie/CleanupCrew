8f5f735b849bb83d9c16af3fadaa6d7f54e81062
Bart <kuijerlattie@gmail.com>
2016-05-25 13:21:16 +0200

    example event listener

---
+++ Unity/CleanupCrew/Assets/Scripts/toolset/eventTest1.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using UnityEngine.Events;
+using System.Collections;
+
+public class eventTest1 : MonoBehaviour
+{
+    private UnityAction someListener;
+
+    void Awake()
+    {
+        someListener = new UnityAction(SomeFunction);
+    }
+
+    void OnEnable()
+    {
+        EventManager.StartListening("testevent1", someListener);
+    }
+
+    void OnDisable()
+    {
+        EventManager.StopListening("testevent1", someListener);
+    }
+
+    void SomeFunction()
+    {
+        Debug.Log("somefunction was called!");
+    }
+}
