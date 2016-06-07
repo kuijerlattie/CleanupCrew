f5054cc6ede359d6266d6729b4341735d07bf965
Bart <kuijerlattie@gmail.com>
2016-06-07 13:40:49 +0200

    first commit for new game

---
+++ Unity/CleanupCrew/Assets/Scripts v2/eventlisteners/point events.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class pointevents : MonoBehaviour
+{
+
+
+    void OnEnable()
+    {
+        EventManager.StartListening("BallBottomDeath", OnBallBottomDeath);
+        EventManager.StartListening("blobBottomDeath", OnBlobBottomDeath);
+        //add all events that you want to listen to when the game starts here.
+    }
+
+    void OnDisable()
+    {
+        EventManager.StopListening("BallBottomDeath", OnBallBottomDeath);
+        EventManager.StopListening("blobBottomDeath", OnBlobBottomDeath);
+        //add all events to stop listening to here
+    }
+
+    void OnBallBottomDeath(GameObject g, float f)
+    {
+        //remove energy
+    }
+
+    void OnBlobBottomDeath(GameObject g, float f)
+    {
+        //remove energy
+    }
+}
