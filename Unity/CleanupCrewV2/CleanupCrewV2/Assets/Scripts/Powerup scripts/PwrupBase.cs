a7606e03410df551ce45cd3d2da1d44d7ddbc8cc
Ari-Pekka Åttman <apattman@gmail.com>
2016-06-09 13:34:41 +0200

    Basics of pwrups and few pwrup

---
+++ Unity/CleanupCrewV2/CleanupCrewV2/Assets/Scripts/Powerup scripts/PwrupBase.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class PwrupBase : MonoBehaviour {
+
+    public abstract void startPwrup();
+
+    public abstract void stopPwrup();
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.collider.tag == "paddle")
+        {
+            startPwrup();
+
+        }
+
+    }
+}
---
+++ Unity/CleanupCrewV2/CleanupCrewV2/Assets/Scripts/Powerup scripts/upScalePwrup.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class upScalePwrup : PwrupBase {
+
+    public override void startPwrup()
+    {
+        Wider();
+    }
+
+    public override void stopPwrup()
+    {
+        ResetScale();
+    }
+
+    static public void Wider()
+    {
+        GameObject paddle;
+
+        paddle = FindObjectOfType<PaddleControls>().gameObject;
+        paddle.transform.localScale = new Vector3(1.5f, 1, 1);
+    }
+
+    static public void ResetScale()
+    {
+        GameObject paddle;
+
+        paddle = FindObjectOfType<PaddleControls>().gameObject;
+        paddle.transform.localScale = new Vector3(1, 1, 1);
+    }
+}
