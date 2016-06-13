4bab761b140be654513131acfa63d70d252c6071
Bart <kuijerlattie@gmail.com>
2016-06-13 12:59:33 +0200

    blob script update
    blobs are now looking at the direction they are moving. also updated
    game documentation

--- Unity/CleanupCrewV2/CleanupCrewV2/Assets/Scripts/LookAtMovementDirection.cs
+++ Unity/CleanupCrewV2/CleanupCrewV2/Assets/Scripts/LookAtMovementDirection.cs
@@ -2,14 +2,8 @@
 using System.Collections;
 
 public class LookAtMovementDirection : MonoBehaviour {
-
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
 	// Update is called once per frame
 	void Update () {
-	
+        transform.LookAt(transform.position + GetComponent<Rigidbody>().velocity);
 	}
 }
