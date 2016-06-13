57ba805c262be44cc706b34b824e275bb136cfd2
jossepr8 <jossepr8@hotmail.com>
2016-06-13 15:58:30 +0200

    sync

--- Unity/CleanupCrewV2/CleanupCrewV2/Assets/Scripts/BlobScript.cs
+++ Unity/CleanupCrewV2/CleanupCrewV2/Assets/Scripts/BlobScript.cs
@@ -88,6 +88,7 @@ public class BlobScript : MonoBehaviour {
     Vector3 GetBehaviourVector()
     {
         //TODO what behaviours?
+        return Vector3.zero; 
         Vector3 vec = Vector3.zero;
         vec.x = Mathf.Sin(transform.position.z);
         vec *= Time.deltaTime * 20f;
