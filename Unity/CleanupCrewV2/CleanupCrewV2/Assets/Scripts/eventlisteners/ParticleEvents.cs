74b508e8b31c342d95ddda7b9e4ab12e64b90b1e
jossepr8 <jossepr8@hotmail.com>
2016-06-16 12:27:28 +0200

    more event for particle script

--- Unity/CleanupCrewV2/CleanupCrewV2/Assets/Scripts/eventlisteners/ParticleEvents.cs
+++ Unity/CleanupCrewV2/CleanupCrewV2/Assets/Scripts/eventlisteners/ParticleEvents.cs
@@ -30,14 +30,79 @@ public class ParticleEvents : MonoBehaviour {
 
     }
 
+    
+
+    void OnBallHitPaddle(GameObject g, float f)
+    {
+
+    }
+
+    void OnRodMoved(GameObject g, float f)
+    {
+        if(f >= 0)
+        {
+            //going up
+
+        }
+        if (f < 0)
+        {
+            //going down
+
+        }
+    }
+
+    void OnBallShoot(GameObject g, float f)
+    {
+
+    }
+
+    void OnStartBoss(GameObject g, float f)
+    {
+        //g = null, use 'new Vector3(x,y,z)' instead of g.transform.position
+
+    }
+
+    void OnGameOver(GameObject g, float f)
+    {
+        //g = null, use 'new Vector3(x,y,z)' instead of g.transform.position
+
+    }
+
+    void OnBlobBottomDeath(GameObject g, float f)
+    {
+
+    }
+
+    
+        
+
+    void OnGainedEnergy(GameObject g, float f)
+    {
+        //g = null, use 'new Vector3(x,y,z)' instead of g.transform.position
+
+    }
+
+    void OnLoseEnergy(GameObject g, float f)
+    {
+        //g = null, use 'new Vector3(x,y,z)' instead of g.transform.position
+
+    }
+
+
     void OnGainedPoints(GameObject g, float f)
     {
+        //g = null, use 'new Vector3(x,y,z)' instead of g.transform.position
 
     }
     void OnLosePoints(GameObject g, float f)
     {
+        //g = null, use 'new Vector3(x,y,z)' instead of g.transform.position
 
     }
+
+
+
+
 
 
 
@@ -53,6 +118,16 @@ public class ParticleEvents : MonoBehaviour {
         EventManager.StartListening("GainedPoints", OnGainedPoints);
         EventManager.StartListening("LosePoints", OnLosePoints);
 
+        EventManager.StartListening("BallHitPaddle", OnBallHitPaddle);
+        EventManager.StartListening("RodMoved", OnRodMoved);
+        EventManager.StartListening("BallShoot", OnBallShoot);
+        EventManager.StartListening("StartBoss", OnStartBoss);
+        EventManager.StartListening("GameOver", OnGameOver);
+        EventManager.StartListening("BlobBottomDeath", OnBlobBottomDeath);
+
+        EventManager.StartListening("GainedEnergy", OnGainedEnergy);
+        EventManager.StartListening("LoseEnergy", OnLoseEnergy);
+
 
     }
 
@@ -67,5 +142,15 @@ public class ParticleEvents : MonoBehaviour {
 
         EventManager.StopListening("GainedPoints", OnGainedPoints);
         EventManager.StopListening("LosePoints", OnLosePoints);
+
+        EventManager.StopListening("BallHitPaddle", OnBallHitPaddle);
+        EventManager.StopListening("RodMoved", OnRodMoved);
+        EventManager.StopListening("BallShoot", OnBallShoot);
+        EventManager.StopListening("StartBoss", OnStartBoss);
+        EventManager.StopListening("GameOver", OnGameOver);
+        EventManager.StopListening("BlobBottomDeath", OnBlobBottomDeath);
+
+        EventManager.StopListening("GainedEnergy", OnGainedEnergy);
+        EventManager.StopListening("LoseEnergy", OnLoseEnergy);
     }
 }
