1ad16ccaf5b9c76dd484886f90773b03ab55f3e6
jossepr8 <jossepr8@hotmail.com>
2016-06-17 15:15:57 +0200

    can choose what UI particles go to

--- Unity/CleanupCrewV2/CleanupCrewV2/Assets/ParticleToUI.cs
+++ Unity/CleanupCrewV2/CleanupCrewV2/Assets/ParticleToUI.cs
@@ -3,11 +3,36 @@ using System.Collections;
 
 public class ParticleToUI : MonoBehaviour {
 
+    public UITarget currentTarget = UITarget.Barrel;
+    public enum UITarget
+    {
+        Barrel,
+        Energy,
+        Points
+    }
+
+    public void Init()
+    {
+        switch(currentTarget)
+        {
+            case UITarget.Barrel:
+                UIWorldPosition = new Vector3(-33, 18, 0);
+                break;
+            case UITarget.Energy:
+                UIWorldPosition = new Vector3(-37, 18, 0);
+                break;
+            case UITarget.Points:
+                UIWorldPosition = new Vector3(-33, 18, -10);
+                Debug.LogWarning("Trying to move particle to Points(score) in UI... where is that in the UI???? ");
+                break;
+        }
+    }
+
     Vector3 UIWorldPosition = new Vector3(-33, 18, 0);
     float speed = 15;
 	// Use this for initialization
 	void Start () {
-
+        gameObject.layer = LayerMask.NameToLayer("OnTopOfUI");
     }
 	
 	// Update is called once per frame
