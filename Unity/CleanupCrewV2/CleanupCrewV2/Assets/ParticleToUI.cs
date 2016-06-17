cc7ddc48c2e5542dd15c1a81b415fc7ef7155e98
jossepr8 <jossepr8@hotmail.com>
2016-06-17 15:00:51 +0200

    Particles that go to the UI

---
+++ Unity/CleanupCrewV2/CleanupCrewV2/Assets/ParticleToUI.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleToUI : MonoBehaviour {
+
+    Vector3 UIWorldPosition = new Vector3(-33, 18, 0);
+    float speed = 15;
+	// Use this for initialization
+	void Start () {
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        transform.position = Vector3.MoveTowards(transform.position, UIWorldPosition, Time.deltaTime * speed);
+        if (Vector3.Distance(transform.position, UIWorldPosition) < Mathf.Epsilon)
+            GameObject.Destroy(gameObject);
+	}
+}
