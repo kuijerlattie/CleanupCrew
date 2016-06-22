d5607b68bdd20cd8861a6e1ef1f0cf2f812d5d5a
Bart <kuijerlattie@gmail.com>
2016-06-22 13:04:31 +0200

    afk implemented
    game goes gameover after 30 seconds of no input, game triggers warning
    after 10 seconds of no input. also uploaded a mole_animation.controller.
    dont know why that wasnt synced yet...
    
    when game is over-> game doesnt load a menu anymore. it waits 5 seconds,
    uploads your score and quits
    
    hud now shows the points without the points: text in front
    
    gamemanager includes afk script

---
+++ Unity/CleanupCrewV2/CleanupCrewV2/Assets/Scripts/AFKScript.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AFKScript : MonoBehaviour {
+
+    float AFKTimer = 0.0f;
+    float AFKWarningTime = 10.0f;
+    float AFKEndGame = 30.0f;
+    bool GaveWarning = false;
+    bool EndedGame = false;
+
+	// Use this for initialization
+	void OnEnable ()
+    {
+        EventManager.StartListening("Click", OnScreenTouch);
+	}
+	
+    void OnDisable()
+    {
+        EventManager.StopListening("Click", OnScreenTouch);
+    }
+
+	// Update is called once per frame
+	void Update () {
+        if (GameManager.instance.CurrentGamestate == GameManager.gamestate.GameOver) return;
+
+        AFKTimer += Time.deltaTime;
+        if (AFKTimer >= AFKWarningTime && !GaveWarning)
+        {
+            EventManager.TriggerEvent("PlayerAFK", null, AFKTimer);
+            GaveWarning = true;
+        }
+
+        if (AFKTimer >= AFKEndGame && !EndedGame)
+        {
+            EventManager.TriggerEvent("PlayerAFKGameOver", null, AFKTimer);
+            EndedGame = true;
+            EventManager.StopListening("Click", OnScreenTouch);
+            GameManager.instance.SetState(GameManager.gamestate.GameOver);
+        }
+	}
+
+    void OnScreenTouch(GameObject g, float f)
+    {
+        AFKTimer = 0.0f;
+        GaveWarning = false;
+    }
+}
