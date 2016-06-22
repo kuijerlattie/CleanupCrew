042ca4f6bf9e5446e2e0af5669248f8c4334ccc6
BoyCkaVlad <enevlad_1994@yahoo.com>
2016-06-22 15:01:12 +0200

    sounds added i think
    sounds added i think

--- Unity/CleanupCrewV2/CleanupCrewV2/Assets/Scripts/eventlisteners/AudiEvents.cs
+++ Unity/CleanupCrewV2/CleanupCrewV2/Assets/Scripts/eventlisteners/AudiEvents.cs
@@ -9,6 +9,26 @@ public class AudiEvents : MonoBehaviour {
 		Debug.Log ("musci played");
 		StaticFuntions.PlaySound (g, "balldead");
 	}
+	void OnBallHitPaddle (GameObject g, float f)
+	{
+		StaticFuntions.PlaySound (g, "ballhitpaddle");
+	}
+	void OnBlobBottomDeath (GameObject g, float f)
+	{
+		StaticFuntions.PlaySound (g, "blolbescaped");
+	}
+	void OnStartBoss (GameObject g, float f)
+	{
+		StaticFuntions.PlaySound (g, "molecomesout");
+	}
+	void OnGameOver (GameObject g, float f)
+	{
+		StaticFuntions.PlaySound (g, "looose");
+	}
+	void OnBallHitRod (GameObject g, float f)
+	{
+		StaticFuntions.PlaySound (g, "powerup");
+	}
 		
 
 	void OnEnable()
@@ -17,18 +37,18 @@ public class AudiEvents : MonoBehaviour {
 		EventManager.StartListening("BallDestroyed", OnBallDestroy);
 		//EventManager.StartListening("BlobSpawn", OnBlobSpawn);
 		//EventManager.StartListening("BallSpawn", OnBallSpawn);
-		//EventManager.StartListening("BallHitRod", OnBallHitRod);
+		EventManager.StartListening("BallHitRod", OnBallHitRod);
 		//EventManager.StartListening("BlobHitRod", OnBlobHitRod);
 		//
 		//EventManager.StartListening("GainedPoints", OnGainedPoints);
 		//EventManager.StartListening("LosePoints", OnLosePoints);
 		//
-		//EventManager.StartListening("BallHitPaddle", OnBallHitPaddle);
+		EventManager.StartListening("BallHitPaddle", OnBallHitPaddle);
 		//EventManager.StartListening("RodMoved", OnRodMoved);
 		//EventManager.StartListening("BallShoot", OnBallShoot);
-		//EventManager.StartListening("StartBoss", OnStartBoss);
-		//EventManager.StartListening("GameOver", OnGameOver);
-		//EventManager.StartListening("BlobBottomDeath", OnBlobBottomDeath);
+		EventManager.StartListening("StartBoss", OnStartBoss);
+		EventManager.StartListening("GameOver", OnGameOver);
+		EventManager.StartListening("BlobBottomDeath", OnBlobBottomDeath);
 		//
 		//EventManager.StartListening("GainedEnergy", OnGainedEnergy);
 		//EventManager.StartListening("LoseEnergy", OnLoseEnergy);
@@ -42,18 +62,18 @@ public class AudiEvents : MonoBehaviour {
 		EventManager.StopListening("BallDestroyed", OnBallDestroy);
 		//EventManager.StopListening("BlobSpawn", OnBlobSpawn);
 		//EventManager.StopListening("BallSpawn", OnBallSpawn);
-		//EventManager.StopListening("BallHitRod", OnBallHitRod);
+		EventManager.StopListening("BallHitRod", OnBallHitRod);
 		//EventManager.StopListening("BlobHitRod", OnBlobHitRod);
 		//
 		//EventManager.StopListening("GainedPoints", OnGainedPoints);
 		//EventManager.StopListening("LosePoints", OnLosePoints);
 		//
-		//EventManager.StopListening("BallHitPaddle", OnBallHitPaddle);
+		EventManager.StopListening("BallHitPaddle", OnBallHitPaddle);
 		//EventManager.StopListening("RodMoved", OnRodMoved);
 		//EventManager.StopListening("BallShoot", OnBallShoot);
-		//EventManager.StopListening("StartBoss", OnStartBoss);
-		//EventManager.StopListening("GameOver", OnGameOver);
-		//EventManager.StopListening("BlobBottomDeath", OnBlobBottomDeath);
+		EventManager.StopListening("StartBoss", OnStartBoss);
+		EventManager.StopListening("GameOver", OnGameOver);
+		EventManager.StopListening("BlobBottomDeath", OnBlobBottomDeath);
 		//
 		//EventManager.StopListening("GainedEnergy", OnGainedEnergy);
 		//EventManager.StopListening("LoseEnergy", OnLoseEnergy);
