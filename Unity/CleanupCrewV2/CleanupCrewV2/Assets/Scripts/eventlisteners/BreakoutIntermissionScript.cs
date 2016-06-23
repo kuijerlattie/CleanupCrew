7597afac440f68bc7d591364c389d3148f2cdf44
Bart <kuijerlattie@gmail.com>
2016-06-23 15:42:49 +0200

    breakout shit added

--- Unity/CleanupCrewV2/CleanupCrewV2/Assets/Scripts/eventlisteners/BreakoutIntermissionScript.cs
+++ Unity/CleanupCrewV2/CleanupCrewV2/Assets/Scripts/eventlisteners/BreakoutIntermissionScript.cs
@@ -8,6 +8,7 @@ public class BreakoutIntermissionScript : BaseGamestate {
         RodScript.EnableAllRods();
         GameManager.instance.SetState(GameManager.gamestate.Breakout);
         GameManager.instance.EmptyBarrelGoo();
+        GameManager.instance.gooNeededForBoss = (int)(4 + (GameManager.instance.CurrentLevel - 1) * 1.25f);
     }
 
     public override void EndState()
