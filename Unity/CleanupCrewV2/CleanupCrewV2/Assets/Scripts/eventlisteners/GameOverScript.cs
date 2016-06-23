b632c8f2a7c8f854f797c786bbf0fab0452e198f
jossepr8 <jossepr8@hotmail.com>
2016-06-23 14:58:50 +0200

    game over screen

--- Unity/CleanupCrewV2/CleanupCrewV2/Assets/Scripts/eventlisteners/GameOverScript.cs
+++ Unity/CleanupCrewV2/CleanupCrewV2/Assets/Scripts/eventlisteners/GameOverScript.cs
@@ -4,18 +4,37 @@ using System.Collections;
 public class GameOverScript : BaseGamestate {
 
     float i = 0;
+    GameObject gameOverObject = null;   //canvas prefab with scaled gameover screen
+
     public override void StartState()
     {
         //explode all things in the scene?
         GameManager.instance.SetGameplayState(GameManager.gameplaystate.paused);
         //make camera flyover the level with blobs going all over the place while your score is shown onscreen?
+        ToggleGameOverScreen(true);
+
     }
 
     public override void EndState()
     {
-
+        
     }
 
+    void ToggleGameOverScreen(bool toggle)
+    {
+        if(toggle && gameOverObject == null)
+        {
+            gameOverObject = GameObject.Instantiate(Resources.Load("prefabs/GameOverCanvas")) as GameObject;
+            gameOverObject.GetComponentInChildren<UnityEngine.UI.Text>().text = GameManager.instance.CurrentPoints.ToString();
+        }
+        else if(gameOverObject != null) //in case you want to restart the game without quitting
+        {
+            GameObject.Destroy(gameOverObject);
+            gameOverObject = null;
+        }
+    }
+
+    
     void Update()
     {
         i += Time.deltaTime;
