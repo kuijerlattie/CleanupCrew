6ed417f54a6884a43046f9e2b85563d4578f8104
Ari-Pekka Åttman <apattman@gmail.com>
2016-06-24 12:51:35 +0200

    Transparent powerups added. (when in launching state)

---
+++ Unity/CleanupCrewV2/CleanupCrewV2/Assets/Scripts/Powerup scripts/transparentPwrups.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class transparentPwrups : MonoBehaviour {
+
+    PaddleControls paddle;
+    MeshRenderer pwrUp;
+
+	// Use this for initialization
+	void Start () {
+        paddle = FindObjectOfType<PaddleControls>();
+        pwrUp = GetComponent<MeshRenderer>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (paddle.currentState == PaddleControls.PaddleState.Launching)
+        {
+            pwrUp.material.color = new Color(pwrUp.material.color.r, pwrUp.material.color.g, pwrUp.material.color.b, 0.25f);
+        }
+        else
+            pwrUp.material.color = new Color(pwrUp.material.color.r, pwrUp.material.color.g, pwrUp.material.color.b, 1);
+
+    }
+}
